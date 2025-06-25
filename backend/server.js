64605b6f515c3f685ed3ae2542e148b9d0ad8359
Raghav Katta <Raghavkatta2005@gmail.com>
2025-06-25 08:49:19 +0530

    feat:added authRoutes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import authRoutes from './routes/authRoutes'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app = express()
+
+app.use('/api/auth', authRoutes)
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => app.listen(process.env.PORT, () => console.log('Server running')))
+    .catch((err) => console.log(err));
